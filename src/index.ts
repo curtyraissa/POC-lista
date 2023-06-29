b5dcf18a1c9704a70f5346121878ed42a5fbfc46
curtyraissa <raissacurty@gmail.com>
2023-06-29 12:56:04 +0100

    adicionado comentarios

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import router from "./routers/itemRouter";
 const app = express();
 app.use(json());
 
+// Rota de verificação de saúde do servidor
 app.get("/health", (req: Request, res: Response) => {
   res.sendStatus(httpStatus.OK);
 });
 
+// Aplica as rotas do itemRouter
 app.use(router);
 
 const port: number = parseInt(process.env.PORT) || 5000;
--- src/routers/itemRouter.ts
+++ src/routers/itemRouter.ts
@@ -4,9 +4,13 @@ import validateSchema from '../middleware/validationMiddleware';
 
 const router = express.Router();
 
+// Rota para criar um novo item
 router.post('/lista', validateSchema, itemController.create);
+// Rota para excluir um item
 router.delete('/lista/:id', itemController.delete);
+// Rota para obter todos os itens
 router.get('/lista', itemController.getAll);
+// Rota para atualizar o status de um item
 router.put('/lista/:id', itemController.updateStatus);
 
 export default router;
