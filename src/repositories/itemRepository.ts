f2c683657c7e324557acdb4373ac36854836cdf4
curtyraissa <raissacurty@gmail.com>
2023-06-30 20:01:26 +0100

    tirando o tratamento de erros do repository

--- src/repositories/itemRepository.ts
+++ src/repositories/itemRepository.ts
@@ -7,12 +7,8 @@ const itemRepository = {
     const query = 'INSERT INTO shopping_list (nome, status) VALUES ($1, $2) RETURNING *';
     const values = [item.nome, item.status];
 
-    try {
-      const result = await connection.query(query, values);
-      return result.rows[0];
-    } catch (error) {
-      throw new Error('Erro ao criar item na lista de compras');
-    }
+    const result = await connection.query(query, values);
+    return result.rows[0];
   },
 
   // Exclui um item da lista de compras
@@ -20,23 +16,15 @@ const itemRepository = {
     const query = 'DELETE FROM shopping_list WHERE id = $1';
     const values = [itemId];
 
-    try {
-      await connection.query(query, values);
-    } catch (error) {
-      throw new Error('Erro ao excluir item da lista de compras');
-    }
+    await connection.query(query, values);
   },
 
   // Obtém todos os itens da lista de compras
   async getAll() {
     const query = 'SELECT * FROM shopping_list';
 
-    try {
-      const result = await connection.query(query);
-      return result.rows;
-    } catch (error) {
-      throw new Error('Erro ao obter itens da lista de compras');
-    }
+    const result = await connection.query(query);
+    return result.rows;
   },
 
 // Atualiza o status de um item na lista de compras
@@ -44,12 +32,8 @@ const itemRepository = {
     const query = 'UPDATE shopping_list SET status = $1 WHERE id = $2 RETURNING *';
     const values = [status, itemId];
 
-    try {
-      const result = await connection.query(query, values);
-      return result.rows[0];
-    } catch (error) {
-      throw new Error('Erro ao atualizar status do item');
-    }
+    const result = await connection.query(query, values);
+    return result.rows[0];
   }
 };
 
